e300805eb7a8120973bab7d80b5be9a32a4f6fbe
isabelabenetti01 <isabela.benetti@escola.pr.gov.br>
2025-05-14 09:23:43 -0300

    fundo do projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'YouTubers',
+    'Qual o nome dos cachorros do Youtuber T3ddy?',
+    'Beto e Jhonny'
+)
+
+criaCartao(
+    'filme',
+    'Qual o filme onde a personagem principal tem cabelo azul?',
+    'Coraline'
+)
+
+criaCartao(
+    'Minecraft',
+    'Como faz uma espada de diamante?',
+    'Usando um graveto e 2 diamantes'
+)
+
+criaCartao(
+    'filmes',
+    'Qual o  filme mais famoso do ator Adam Sandler',
+    'Como Se Fosse a Primeira Vez. '
+)
